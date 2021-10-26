71e4adf5747dbed964d934eb43c2a26b4fbd4091
NguyenDuong-GCH0804 <84314118+NguyenDuong-GCH0804@users.noreply.github.com>
2021-10-26 22:22:39 +0700

    Update

--- src/route/index.js
+++ src/route/index.js
@@ -9,7 +9,6 @@ const router = express.Router();
 
 // router.use('/admin', admin);
 router.use('/trainer',authenticate.verifyUser, authenticate.isTrainer, trainer);
-router.use('/profile', authenticate.verifyUser,  profile);
 router.use('/staff',authenticate.verifyUser, authenticate.isStaff, staff);
 
 router.get('/upload', (req, res, next) => {
