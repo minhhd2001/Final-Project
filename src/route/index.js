a54834d5569153f4715a9b8a52a8520f2b90349f
bombom28821 <bombom28821@gmail.com>
2021-10-27 15:48:59 +0700

    update index/router

--- src/route/index.js
+++ src/route/index.js
@@ -7,17 +7,13 @@ const staff = require('./staff.route');
 const authenticate = require('../middleware/auth.middleware')
 const router = express.Router();
 
-// router.use('/admin', admin);
-router.use('/trainer',authenticate.verifyUser, authenticate.isTrainer, trainer);
-router.use('/profile', authenticate.verifyUser,  profile);
-router.use('/staff',authenticate.verifyUser, authenticate.isStaff, staff);
+router.use('/admin', authenticate.verifyUser, authenticate.isAdmin, admin);
+router.use('/trainer', authenticate.verifyUser, authenticate.isTrainer, trainer);
+router.use('/profile', authenticate.verifyUser, profile);
+router.use('/staff', authenticate.verifyUser, authenticate.isStaff, staff);
 
-router.get('/upload', (req, res, next) => {
-    res.render('upload')
-})
+
 router.use('/', auth);
-// router.use('/:slug', (req, res, next) => {
-//     res.send('Not found page')
-// });
+
 
 module.exports = router;
